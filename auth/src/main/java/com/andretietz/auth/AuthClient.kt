c19edee981e96700a7999759921adb2495d17782
André Tietz <andre.tietz@gmail.com>
2017-12-10 15:53:36 +0100

    introduced sign in state observable

--- auth/src/main/java/com/andretietz/auth/AuthClient.kt
+++ auth/src/main/java/com/andretietz/auth/AuthClient.kt
@@ -1,11 +1,15 @@
 package com.andretietz.auth
 
 import io.reactivex.Maybe
+import io.reactivex.Observable
 import io.reactivex.Single
 
 interface AuthClient<RESULT> {
-    fun isSignedIn(): Maybe<RESULT>
+    fun getUser(): Maybe<RESULT>
+    fun signInState(): Observable<State<RESULT>>
     fun signUp(credential: AuthCredential): Single<RESULT>
     fun signIn(credential: AuthCredential): Single<RESULT>
     fun signOut(): Single<RESULT>
+
+    data class State<out RESULT>(val user: RESULT?)
 }
